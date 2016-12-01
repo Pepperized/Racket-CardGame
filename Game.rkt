c3740989e098cd8d7f135cbc28c1e676691716c9
Brad <bradley.stapleton@tesco.net>
2016-12-01 10:29:32 +0000

    Start of project

---
+++ Game.rkt
@@ -0,0 +1,40 @@
+#lang racket/gui
+
+(require racket/draw
+         net/url)
+
+(define logo
+  (read-bitmap (get-pure-port (string->url "http://racket-lang.org/logo.png"))))
+
+
+(define frame (new frame% [label "Window"] [width 1400] [height 800]))
+
+(define canvas (new canvas% [parent frame]))
+
+(void (new message% [parent frame] [label logo]))
+
+ 
+; Make a static text message in the frame
+(define msg (new message% [parent frame]
+                          [label "No events so far..."]))
+ 
+; Make a button in the frame
+(new button% [parent frame]
+             [label "Click Me"]
+             ; Callback procedure for a button click:
+             [callback (lambda (button event)
+                         (send msg set-label "Button click"))])
+
+(define menu-bar (new menu-bar%
+                      (parent frame)))
+
+(define file (new menu%
+     (label "&File")
+     (parent menu-bar)))
+
+(define choice1 (new menu-item%
+                     [parent file] [label "New Game"] [callback (lambda (button event)
+                         (send msg set-label "Button click"))]))
+ 
+; Show the frame by calling its show method
+(send frame show #t)
