e6445273b93f995156fc5ea74d9a99cc238fe619
Brad Stapleton <bradley.stapleton@tesco.net>
2017-01-23 17:32:17 +0000

    Helpers file

---
+++ Helpers.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(define (index-of lst ele)
+  (let loop ((lst lst)
+             (idx 0))
+    (cond ((empty? lst) #f)
+          ((equal? (first lst) ele) idx)
+          (else (loop (rest lst) (add1 idx))))))
+
+(provide index-of)
